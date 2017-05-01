fc8650f0a3839bc32f878c8a5b5ac6f973a99139
ricster101 <ricardo.margarido94@gmail.com>
2017-05-01 19:29:07 +0100

    Changed the way to visualize data, less clunky and easier to see important topics

--- start.py
+++ start.py
@@ -1,5 +1,17 @@
 import os
 
-with open(os.path.join('data', "movies.txt")) as infile:
-    for line in infile:
+
+c = 0
+singleReview = []
+with open(os.path.join('data', "movies.txt")) as file:
+    for line in file:
         print(line)
+        singleReview.append(line)
+        c +=1
+        if c%9 == 0:
+            print(singleReview)
+            singleReview.clear()
+            breaker = input("Press Enter to continue, type X to close...")
+            if breaker == 'X' or 'x':
+                break
+            print("\n" * 100)
