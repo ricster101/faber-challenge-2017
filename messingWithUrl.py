961a37c3b5f13dca6709c162bd10e4450640f405
ricster101 <ricardo.margarido94@gmail.com>
2017-05-04 03:20:30 +0100

    Extracted genres, when available Getting errors 503 because I cant auto acess the amazon website

--- messingWithUrl.py
+++ messingWithUrl.py
@@ -11,8 +11,8 @@ def cleanhtml(raw_html):
 
 
 #TODO getting a for cicle here to query every link available and extract at least the genre
-
-url = 'https://www.amazon.com/dp/B000ICXQHY'
+'''
+url = 'https://www.amazon.com/dp/B008D9NIHW'
 
 u = urllib.request.urlopen(url)
 
@@ -30,3 +30,36 @@ xxxx = [name for name in xxx if name.strip()]
 for i,j in enumerate(xxxx):
     if 'Genres' in j:
         print (xxxx[i+1])
+        ecsdee = " ".join(xxxx[i + 1].split())
+        print(ecsdee)
+
+'''
+
+processed = open('dataProcessed/reviewGreater50.txt', 'r')
+processed = processed.read()
+
+procList = processed.split('\n')
+productGenred = set()
+
+c = 0
+for i in procList:
+    if i.startswith('product'):
+        if i not in productGenred:
+            productGenred.add(i)
+            print(i.split(':')[1].strip())
+            url = 'https://www.amazon.com/dp/' + i.split(':')[1].strip()
+            u = urllib.request.Request(url, None, {'User-agent' : 'Chrome/44.0.2403.107'})
+
+            opening = urllib.request.urlopen(u).read().decode('utf-8')
+            opening = cleanhtml(opening)
+            uSplit = opening.split('\n')
+            uNoSpace = [line for line in uSplit if line.strip()]
+
+            for k, j in enumerate(uNoSpace):
+                if 'Genres' in j:
+                    genre = " ".join(uNoSpace[k + 1].split())
+                    print(genre)
+            print('-'*20)
+        c += 1
+    if (c == 2):
+        break
